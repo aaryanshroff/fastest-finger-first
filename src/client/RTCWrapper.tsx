2c3ff1785b603085a05d3e22a6ea72d994614938
Aaryan Shroff <aaryanshroff@v1040-wn-rt-b-37-202.campus-dynamic.uwaterloo.ca>
2022-05-09 17:17:15 -0400

    Console log event object

--- src/client/RTCWrapper.tsx
+++ src/client/RTCWrapper.tsx
@@ -41,7 +41,7 @@ const RTCWrapper: React.FC<IRTCWrapperProps> = () => {
   const handleTrackEvent = (event: RTCTrackEvent) => {
     if (remoteVideoRef) {
       console.log("Received remote stream");
-      console.log(event.streams[0]);
+      console.log(event);
       remoteVideoRef.current.srcObject = event.streams[0];
     }
   };
